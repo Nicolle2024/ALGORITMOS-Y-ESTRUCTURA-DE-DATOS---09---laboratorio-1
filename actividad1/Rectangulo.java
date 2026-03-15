5e623f8de82f2b382e2b76b92b91302e17b1b069
Nicolle2024 <nicolle.castilla@gmail.com>
2026-03-14 19:28:29 -0500

    Add Rectangulo class with corner coordinates

---
+++ actividad1/Rectangulo.java
@@ -0,0 +1,30 @@
+public class Rectangulo {
+
+    private Coordenada esquina1;
+    private Coordenada esquina2;
+
+    public Rectangulo(Coordenada c1, Coordenada c2) {
+        setEsquina1(c1);
+        setEsquina2(c2);
+    }
+
+    public void setEsquina1(Coordenada coo) {
+        esquina1 = new Coordenada(coo);
+    }
+
+    public void setEsquina2(Coordenada coo) {
+        esquina2 = new Coordenada(coo);
+    }
+
+    public Coordenada getEsquina1() {
+        return esquina1;
+    }
+
+    public Coordenada getEsquina2() {
+        return esquina2;
+    }
+
+    public String toString() {
+        return "Rectangulo: Esquina1 = " + esquina1 + ", Esquina2 = " + esquina2;
+    }
+}
